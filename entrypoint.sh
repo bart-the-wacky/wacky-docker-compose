b4ead5d519a0a55a05f4271860e9f77c7299fe5d
bart-the-wacky <bart_the_wacky@hotmail.com>
2019-08-05 10:29:20 +0200

    tentative de debug chrome/git

---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+cd /usr/share
+git clone https://github.com/bart-the-wacky/wacky-docker-compose.git
+cp -R /usr/share/wacky-docker-compose/jenkins_home /var/jenkins_home
+
+echo '------------------- Traitement terminé, Jenkins clôné! -----------------------------'
